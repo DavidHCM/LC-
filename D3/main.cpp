16977d45f4b66d7581cffde5b5923fa79ac4aaca
David Hernández <david.hernandezc@iteso.mx>
2023-05-26 23:13:46 -0600

    IV res

--- D3/main.cpp
+++ D3/main.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
+    /*
     int n1,n2, res;
     std::cout << "Num 1:";
     std::cin >> n1;
@@ -19,6 +20,14 @@ int main()
     res = 0;
     res = n1 / n2;
     std::cout << "Res 1:" <<res<<std::endl;
+    */
+    float s1,res,IV = 16;
+    std::cout << "Price:";
+    std::cin >> s1;
+    res = s1 * IV;
+
+    std::cout << "res:" <<res<<std::endl;
+
 
     return 0;
 }
