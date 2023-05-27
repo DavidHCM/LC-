d96dc622afad83d56928c6d17e6ad1e9a1541db1
David Hernández <david.hernandezc@iteso.mx>
2023-05-26 23:11:49 -0600

    SRMD

--- D3/main.cpp
+++ D3/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    int n1,n2, res;
+    std::cout << "Num 1:";
+    std::cin >> n1;
+    std::cout << "Num 2:";
+    std::cin >> n2;
+
+    res = n1 + n2;
+    std::cout << "Res 1:" <<res<<std::endl;
+    res = 0;
+    res = n1 - n2;
+    std::cout << "Res 1:" <<res<<std::endl;
+    res = 0;
+    res = n1 * n2;
+    std::cout << "Res 1:" <<res<<std::endl;
+    res = 0;
+    res = n1 / n2;
+    std::cout << "Res 1:" <<res<<std::endl;
+
     return 0;
 }
