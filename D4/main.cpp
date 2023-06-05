9c521417593787482eb2d42c4ec820213a4d3cb6
David Hernández <david.hernandezc@iteso.mx>
2023-06-04 23:48:36 -0600

    3

--- D4/main.cpp
+++ D4/main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-
+    /*
     string name = "gri Academy";
     // Funcion de .find busca un parametro donde queremos un caracter o string
     cout << name.find("Academy",0) << endl;
@@ -11,7 +11,16 @@ int main() {
 
     // Aqui tomando dos parametros nos dara elementos de un cierto string y tambien podemos pedir la longitud a tomar
     cout << name.substr(4,3) << endl; // Nos dara: Aca
-    
+     */
+
+    /*
+    string name;
+    cout << "name: ";
+    getline(cin,name); // Para tomar toda la linea del user, donde mandamos como parametro el cin que entrara y la variable a almacenar
+    // De solo necesitar un elemento de char entonces solo usar el cin
+    cout << name;
+     */
+
 
 
     return 0;
