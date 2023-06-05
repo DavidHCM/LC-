a9ac83cd39c9d91783e7677e49047231ba62ad70
David Hernández <david.hernandezc@iteso.mx>
2023-06-04 23:40:07 -0600

    1

--- D4/main.cpp
+++ D4/main.cpp
@@ -3,6 +3,15 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+
+    string name = "gri Academy";
+    // Funcion de .find busca un parametro donde queremos un caracter o string
+    cout << name.find("Academy",0) << endl;
+    // Aqui usando este ejemplo me regresa el index donde lo encontro
+
+    // Aqui tomando dos parametros nos dara elementos de un cierto string y tambien podemos pedir la longitud a tomar
+    cout << name.substr(4,3) << endl;
+
+
     return 0;
 }
