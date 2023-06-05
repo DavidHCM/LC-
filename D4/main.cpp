b4e5666da64d974f24312cc067accbb35d4878e7
David Hernández <david.hernandezc@iteso.mx>
2023-06-04 23:42:16 -0600

    2

--- D4/main.cpp
+++ D4/main.cpp
@@ -10,7 +10,8 @@ int main() {
     // Aqui usando este ejemplo me regresa el index donde lo encontro
 
     // Aqui tomando dos parametros nos dara elementos de un cierto string y tambien podemos pedir la longitud a tomar
-    cout << name.substr(4,3) << endl;
+    cout << name.substr(4,3) << endl; // Nos dara: Aca
+    
 
 
     return 0;
