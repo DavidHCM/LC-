61ca84186588e5a3adbb4055ae75357f6b6472fb
David Hernández <david.hernandezc@iteso.mx>
2023-06-06 23:18:17 -0600

    lGame

--- D5/main.cpp
+++ D5/main.cpp
@@ -2,7 +2,36 @@
 
 using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    /*
+    // Madlibs
+    // Enter values with a console in
+
+    // This part is just to remember some stuff
+    string color, noun,someone;
+
+    cout << "Enter color:";
+    // getline to get a string
+    getline(cin,color);
+
+    cout << "Enter a noun:";
+    getline(cin,noun);
+
+    cout << "Enter someone:";
+    getline(cin,someone);
+
+    // We get all the values in the next story
+    cout << "Roses are " << color << endl;
+    cout << noun << " are blue" << endl;
+    cout << "I love " << someone << endl;
+     */
+
+    // Arrays
+
+    // The arrays are similar to C arrays, with the same sintaxis
+
+    int numsLuc[] = {1,2,3,4,5,6};
+
     return 0;
 }
