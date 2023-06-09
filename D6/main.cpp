b1a217fdf4a47fabaa875a3663e00caa26a48da4
David Hernández <david.hernandezc@iteso.mx>
2023-06-08 19:35:54 -0600

    constructor

--- D6/main.cpp
+++ D6/main.cpp
@@ -2,7 +2,35 @@
 
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
+// Class
+class Book
+{
+public:
+    string title;
+    string author;
+    int pages;
+
+    // Constructor
+    // The constructor goes inside the class declaration
+    Book(string name)
+    {
+        // This constructor is executing each time we call the function
+        cout << "Creating object: " << name << endl;
+    }
+};
+
+
+
+
+
+int main()
+{
+    Book book1("Harry potter");
+    book1.title = "Harry Potter";
+    book1.author = "JK Rowling";
+    book1.pages = 300;
+
+
+
     return 0;
 }
