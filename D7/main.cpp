7f29bd36cbf2a4a5695580d7f167552443f0b657
David Hernández <david.hernandezc@iteso.mx>
2023-07-12 18:19:55 -0600

    Same thign with D7, cmake is not working

---
+++ D7/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+// Symbolic constant
+// const double pi = 3.141516;  Read only variable
+
+int main()
+{
+    
+
+
+
+    return 0;
+}
